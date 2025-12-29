2c35530a0767b1e0e16c203b0505be43c4e6b6c5
Tony Le <spiritphyz@users.noreply.github.com>
2025-12-29 12:31:23 -0500

    fix: PHP Blade directives, parameters

---
+++ User/.config/nvim/after/queries/blade/highlights.scm
@@ -0,0 +1,31 @@
+; Blade-specific highlighting (minimal inheritance of html,php to avoid conflicts)
+;extends
+
+; Blade directives
+(directive_start) @keyword
+(directive_end) @keyword
+(directive) @keyword
+(conditional_keyword) @keyword
+
+; Parameters
+(parameter) @variable
+
+; Comments
+(comment) @comment
+
+; PHP
+(php_statement) @keyword
+(php_only) @keyword
+
+; Constructs
+(loop) @repeat
+(conditional) @conditional
+
+; HTML tags
+(tag_name) @tag
+(attribute_name) @property
+(attribute_value) @string
+
+; Content
+(text) @none
+(entity) @constant
