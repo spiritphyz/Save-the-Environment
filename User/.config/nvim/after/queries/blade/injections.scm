cec0aca968f79b72605dea16152382eabcb5308c
Tony Le <spiritphyz@users.noreply.github.com>
2026-05-09 14:42:42 -0400

    fix(blade): injections for nvim 0.12
    Neovim 0.12 now has built-in support for Tree-sitter, so no separate plugin is required. Also, the external plugin was deprecated as "read-only".
    
    Old deprecated plugin:
    https://github.com/nvim-treesitter/nvim-treesitter
    
    Fork of deprecated plugin, now is a community registry of parses:
    https://github.com/neovim-treesitter/nvim-treesitter

--- User/.config/nvim/after/queries/blade/injections.scm
+++ User/.config/nvim/after/queries/blade/injections.scm
@@ -1,16 +1,11 @@
-; Inject PHP syntax into blade parameters
-((parameter) @injection.content
-  (#set! injection.language "php_only")
-  (#set! injection.include-children))
+;extends
 
-; Inject PHP into php_statement blocks
-((php_statement) @injection.content
-  (#set! injection.language "php"))
+; JavaScript in script tags
+((script_element
+  (raw_text) @injection.content)
+  (#set! injection.language "javascript"))
 
-; Inject PHP into php_only blocks
-((php_only) @injection.content
-  (#set! injection.language "php"))
-
-; Inject HTML into the document
-((text) @injection.content
-  (#set! injection.language "html"))
+; CSS in style tags
+((style_element
+ (raw_text) @injection.content)
+ (#set! injection.language "css"))
